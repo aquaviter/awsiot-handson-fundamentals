78db2bee6a931ccad1af91e9f60659188a2d78d1
tsuzy <yoshikazu.tsuji@gmail.com>
2015-12-31 18:31:53 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# awsiot-handson-enhanced documentation build configuration file, created by
+# awsiot-handson-fundamentals-c documentation build configuration file, created by
 # sphinx-quickstart on Sat Oct 31 23:27:37 2015.
 #
 # This file is execfile()d with the current directory set to its
@@ -43,7 +43,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'awsiot-handson-enhanced'
+project = u'awsiot-handson-fundamentals-c'
 copyright = u'2015, aquaviter'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -176,7 +176,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'awsiot-handson-enhanceddoc'
+htmlhelp_basename = 'awsiot-handson-fundamentals-c-doc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -196,7 +196,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'awsiot-handson-enhanced.tex', u'awsiot-handson-enhanced Documentation',
+  ('index', 'awsiot-handson-fundamentals-c.tex', u'awsiot-handson-fundamentals-c Documentation',
    u'aquaviter', 'manual'),
 ]
 
@@ -226,7 +226,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'awsiot-handson-enhanced', u'awsiot-handson-enhanced Documentation',
+    ('index', 'awsiot-handson-fundamentals-c', u'awsiot-handson-fundamentals-c Documentation',
      [u'aquaviter'], 1)
 ]
 
@@ -240,8 +240,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'awsiot-handson-enhanced', u'awsiot-handson-enhanced Documentation',
-   u'aquaviter', 'awsiot-handson-enhanced', 'One line description of project.',
+  ('index', 'awsiot-handson-fundamentals-c', u'awsiot-handson-fundamentals-c Documentation',
+   u'aquaviter', 'awsiot-handson-fundamentals-c', 'One line description of project.',
    'Miscellaneous'),
 ]
 
